a1392c18f06d43779fdc2d0a76c04f7a1aa6fc49
ahad <md.ahadulalam@gmail.com>
2024-08-01 09:22:04 +0600

    GridItems added

--- src/components/GridItems.js
+++ src/components/GridItems.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import '../css/GridItems.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 
 
 const GridItems = () => {
