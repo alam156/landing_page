0a96f9c3c313fca6fa9c623590f42264341865b8
ahad <md.ahadulalam@gmail.com>
2024-08-29 12:43:10 +0600

    about portion updated

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,54 @@
 import React from "react";
+import '../css/about.css';
+
+export const About = (props) => {
+  return (
+      <div id="about">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6 mb-4 mb-md-0">
+              <img
+                  src="./img/about.jpg"
+                  className="img-fluid"
+                  alt=""
+              />
+            </div>
+            <div className="col-md-6">
+              <div className="about-text">
+                <h2>About Us</h2>
+                <p>{props.data ? props.data.paragraph : "Loading..."}</p>
+                <h3>Why Choose Us?</h3>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <ul>
+                      {props.data
+                          ? props.data.Why.map((d, i) => (
+                              <li key={`${d}-${i}`}>{d}</li>
+                          ))
+                          : "Loading"}
+                    </ul>
+                  </div>
+                  <div className="col-sm-6">
+                    <ul>
+                      {props.data
+                          ? props.data.Why2.map((d, i) => (
+                              <li key={`${d}-${i}`}>{d}</li>
+                          ))
+                          : "Loading"}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+
+/*
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const About = (props) => {
@@ -28,9 +78,9 @@ export const About = (props) => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-              {/*<h2>About Us</h2>
+              {/!*<h2>About Us</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>*/}
+              <h3>Why Choose Us?</h3>*!/}
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
@@ -49,3 +99,4 @@ export const About = (props) => {
     </div>
   );
 };
+*/
