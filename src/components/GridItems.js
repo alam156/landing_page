6fdd3347c7322701562fa94c30e1da547b1b4024
ahad <md.ahadulalam@gmail.com>
2024-08-29 10:34:14 +0600

    change in features

--- src/components/GridItems.js
+++ src/components/GridItems.js
@@ -5,7 +5,7 @@ import '../css/GridItems.css';
 const GridItems = () => {
     const gridItems = [
         {
-            icon: require('./icons/icon1.png'),
+            icon: './icons/icon1.png',
             title: 'Local Certificate & Signing',
             description: 'BCC provides users an easy to use online portal \n' +
                 '(bcc-ca.gov.bd) where user can apply for certificates for their \n' +
@@ -17,7 +17,7 @@ const GridItems = () => {
                 'business applications can easily enable local signing in their workflow.'
         },
         {
-            icon: require('./icons/icon2.png'),
+            icon: './icons/icon2.png',
             title: 'Identity Certificate',
             description: 'Identity Certificate is mainly used to prove someone’s identity \n' +
                 'in any information system or in any system the internet or digital space.\n' +
@@ -25,7 +25,7 @@ const GridItems = () => {
                 ' or any online form to prove that he/she knowingly sign this with his/her legally valid digital identity.'
         },
         {
-            icon: require('./icons/icon3.png'),
+            icon: './icons/icon3.png',
             title: 'SSL Client Authority Certificate',
             description: 'SSL Client Authentication certificate is used along with the SSL Server \n' +
                 'certificate to allow users to log in to any system using TLS authentication.\n' +
@@ -34,21 +34,21 @@ const GridItems = () => {
                 'two factor authentication of a system and to strengthen the authentication system of a system.'
         },
         {
-            icon: require('./icons/icon4.png'),
+            icon: './icons/icon4.png',
             title: 'Code Signing Certificate',
             description: 'Code Signing Certificate is used mainly by the Publisher of a Software to ensure the authenticity \n' +
                 'of their Software. All trusted software is usually digitally signed with code signing certificates. \n' +
                 'This certificate is popular among Software companies.'
         },
         {
-            icon: require('./icons/icon5.png'),
+            icon: './icons/icon5.png',
             title: 'Encryption Certificate',
             description: 'Encryption certificate is used to encrypt any files, directory, file systems, partition or devices).\n' +
                 'BCC CA provides key escrow service only for this kind of service and this is only available \n' +
                 'under Class 3 i.e. after physical verification.'
         },
         {
-            icon: require('./icons/icon6.png'),
+            icon: './icons/icon6.png',
             title: 'Timestamping Services provided by BCC',
             description: 'Timestamping can be used to independently and irrefutably prove the time of a transaction, \n' +
                 'the time a document was signed and when it was archived. BCC provides timestamping as an API service.'
@@ -61,20 +61,51 @@ const GridItems = () => {
                 {gridItems.map((item, index) => (
                     <div className="col-md-4 mb-4" key={index}>
                         <div className="grid-item border-top-green">
-                            <img
-                                src={item.icon}
-                                alt=""
-                                className="img-fluid"
-                                style={{ width: '30%', height: '50%' }}
-                            />
-
-                            <h4 className="grid-title">{item.title}</h4>
-                            <p className="grid-description">{item.description}</p>
+                            <div className="row justify-content-center">
+                                <img
+                                    src={item.icon}
+                                    alt=""
+                                    className="img-fluid"
+                                    style={{ width: '100px', height: '100px' }} // Fixed size for the image
+                                />
+                            </div>
+                            <div>
+                                <h4 className="grid-title" style={{ minHeight: '50px' }}>
+                                    {item.title}
+                                </h4>
+                                <p className="grid-description" style={{ minHeight: '70px' }}>
+                                    {item.description}
+                                </p>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
+        /*<div className="grid-items">
+            <div className="row">
+                {gridItems.map((item, index) => (
+                    <div className="col-md-4 mb-4" key={index}>
+                        <div className="grid-item border-top-green">
+                            <div className="row justify-content-center">
+                                <img
+                                    src={item.icon}
+                                    alt=""
+                                    className="img-fluid"
+                                    style={{ width: '30%', height: '50%' }}
+                                />
+
+                            </div>
+                            <div>
+                                <h4 className="grid-title">{item.title}</h4>
+                                <p className="grid-description">{item.description}</p>
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>*/
     );
 };
 
--- src/components/features.js
+++ src/components/features.js
@@ -1,15 +1,53 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import { faComments, faBullhorn, faAward, faBasketball,faPeopleGroup, faMagic} from '@fortawesome/free-solid-svg-icons';
+import '../css/features.css';
+const iconMapping = {
+    "fa fa-comments-o": faComments,
+    "fa fa-bullhorn": faBullhorn,
+    "fa fa-basketball": faBasketball,
+    "fa fa-award": faAward,
+    "fa fa-group": faPeopleGroup,
+    "fa fa-magic": faMagic
+    // Add more mappings as needed
+};
 export const Features = (props) => {
   return (
     <div id="features" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Features</h2>
-        </div>
-        <div className="row">
+          <div className="row">
+              <div className="col-md-3">
+
+              </div>
+              <div className="col-md-6">
+                  <h2>Features</h2>
+              </div>
+              <div className="col-md-3">
+
+              </div>
+
+          </div>
+          <div className="row">
+              {props.data ? (
+                  props.data.map((d, i) => (
+                      <div key={`${d.title}-${i}`} className="col-md-6 col-lg-4 mb-4">
+                          <div className="card h-100 text-center border-0 shadow-sm p-4 custom-card">
+                              <FontAwesomeIcon
+                                  icon={iconMapping[d.icon]}
+                                  size="3x"
+                                  className="mb-3 custom-icon"
+                              />
+                              <h3 className="h5 font-weight-bold text-black">{d.title}</h3>
+                              <p className="text-muted text-black-50">{d.text}</p>
+                          </div>
+                      </div>
+                  ))
+              ) : (
+                  "Loading..."
+              )}
+          </div>
+        {/*<div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-md-6 col-md-3">
@@ -20,7 +58,7 @@ export const Features = (props) => {
                 </div>
               ))
             : "Loading..."}
-        </div>
+        </div>*/}
       </div>
     </div>
   );
