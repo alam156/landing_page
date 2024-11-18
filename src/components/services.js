2086fd9f71d9ab8df7ead86159747a09820c18bc
sourav0408 <souravhsn1@gmail.com>
2024-11-18 11:59:49 +0600

    Feature for our services

--- src/components/services.js
+++ src/components/services.js
@@ -1,7 +1,83 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/services.css';
 import { faCannabis, faCrown, faCubes, faCamera, faUserClock, faCalendar } from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Faster from '../album/Faster.png';
+import HassleFree2 from '../album/HassleFree2.png';
+import LegallyBinding from '../album/legallyBinding.png';
+
+export const Services = (props) => {
+  const iconLookup = {
+    icon1: faCalendar,
+    icon2: faCrown,
+    icon3: faCubes,
+    icon4: faCamera,
+    icon5: faUserClock,
+    icon6: faCannabis
+  };
+
+  const imageStyle = {
+    width: '90%',
+    height: '60vh',
+    borderRadius: '10px',
+    transition: 'transform 0.3s ease, filter 0.3s ease',
+    boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.3)',
+    filter: 'brightness(1.1) saturate(1.2)',
+    cursor: 'pointer'
+  };
+
+  return (
+      <div id="services" className="text-center" style={{ paddingLeft: '3%', paddingRight: '3%' }}>
+        <div className="container-fluid">
+          <div className="section-title">
+            <h2>Our Services</h2>
+          </div>
+          <div className="container-fluid">
+            <div className="row text-center">
+              <div className="col-12 col-md-4 mb-4">
+                <img
+                    src={Faster}
+                    style={imageStyle}
+                    className="vibrant-hover"
+                    alt="Image 1"
+                />
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img
+                    src={HassleFree2}
+                    style={imageStyle}
+                    className="vibrant-hover"
+                    alt="Image 2"
+                />
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img
+                    src={LegallyBinding}
+                    style={imageStyle}
+                    className="vibrant-hover"
+                    alt="Image 3"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+
+
+
+
+
+/*import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faCannabis, faCrown, faCubes, faCamera, faUserClock, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import Faster from '../album/Faster.jpg';
+import HassleFree from '../album/hasslefree.jpg';
+import LegallyBinding from '../album/legallyBinding.jpg';
+
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Services = (props) => {
   const iconLookup = {
@@ -21,7 +97,68 @@ export const Services = (props) => {
               We provide Services for customer satisfaction.
             </p>
           </div>
-          <div className="row">
+          <div className="container">
+            <div className="row text-center">
+              <div className="col-12 col-md-4 mb-4">
+                <img src={Faster} className="img-fluid rounded" alt="Image 1"/>
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img src={HassleFree} className="img-fluid rounded" alt="Image 2"/>
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img src={LegallyBinding} className="img-fluid rounded" alt="Image 3"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};*/
+{/*import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { faCannabis, faCrown, faCubes, faCamera, faUserClock, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import Faster from '../album/Faster.jpg';
+import HassleFree from '../album/hasslefree.jpg';
+import LegallyBinding from '../album/legallyBinding.jpg';
+
+//import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+export const Services = (props) => {
+  const iconLookup = {
+    icon1: faCalendar,
+    icon2: faCrown,
+    icon3: faCubes,
+    icon4: faCamera,
+    icon5: faUserClock,
+    icon6: faCannabis
+  };
+  return (
+      <div id="services" className="text-center">
+
+
+        <div className="container">
+          <div className="section-title">
+            <h2>Our Services</h2>
+            <p>
+              We provide Services for customer satisfaction.
+            </p>
+          </div>
+          <div className="container">
+            <div className="row text-center">
+              <div className="col-12 col-md-4 mb-4">
+                <img src={Faster} className="img-fluid rounded" alt="Image 1"/>
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img src={HassleFree} className="img-fluid rounded" alt="Image 2"/>
+              </div>
+              <div className="col-12 col-md-4 mb-4">
+                <img src={LegallyBinding} className="img-fluid rounded" alt="Image 3"/>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        {/*          <div className="row">
             {props.data
                 ? props.data.map((d, i) => (
                     <div key={`${d.name}-${i}`} className="col-md-4">
@@ -35,10 +172,11 @@ export const Services = (props) => {
                 ))
                 : "loading"}
           </div>
-        </div>
       </div>
+
+
   );
-};
+};*/}
 //Runing code api
 /*
 import React, { useEffect, useState } from "react";
